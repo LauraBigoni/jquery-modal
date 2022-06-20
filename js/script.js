52c82609e9609a937c8dfb789c59aafb23dd713d
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-20 15:36:22 +0200

    add close modal

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ $(document).ready(function () {
   // Prendo i button di apertura nel DOM e intercetto il click
   $('.open-modal').click(function () {
 
-    // Salvo in una variabile 
+    // Salvo in una variabile l'id per utilizzarlo
+    // dinamicamente per rimuovere la classe della modale giusta
     const modalId = $(this).data('modal');
     console.log(modalId);
+    $('#' + modalId).removeClass('modal--closed');
   });
 
   // Prendo i button di chiusura nel DOM e intercetto il click
   $('.modal__close').click(function () {
 
-    // 
+    // Riaggiungo la classe della chiusura modale
+    const modalClose = $(this).parents('.modal');
+    console.log(modalClose);
+    modalClose.addClass('modal--closed');
   });
 });
